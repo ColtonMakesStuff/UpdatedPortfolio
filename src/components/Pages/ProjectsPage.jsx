13ba6e879ad455cb66baf01f96e54ee39d7e67cc
Colton Firestone <colton.firestone@gmail.com>
2024-02-15 15:26:00 -0800

    finished adding project

--- src/components/Pages/ProjectsPage.jsx
+++ src/components/Pages/ProjectsPage.jsx
@@ -13,11 +13,11 @@ function ProjectsPage() {
     <h1 style={{textAlign:'center'}}>Projects</h1>
     <DividerHorizontal />
 
+    <MainContentCard data={projectCards[0]} direction={'left'}/>
+    <DividerHorizontal />
     <MainContentCard data={projectCards[1]} direction={'left'}/>
     <DividerHorizontal />
     <MainContentCard data={projectCards[6]} direction={'left'}/>
-    <DividerHorizontal />
-    <MainContentCard data={projectCards[0]} direction={'left'}/>
     <DividerHorizontal />
     <MainContentCard data={projectCards[2]} direction={'left'}/>
     <DividerHorizontal />
@@ -26,7 +26,8 @@ function ProjectsPage() {
     <MainContentCard data={projectCards[4]} direction={'left'}/>
     <DividerHorizontal />
     <MainContentCard data={projectCards[5]} direction={'left'}/>
-   
+    <DividerHorizontal />
+    <MainContentCard data={projectCards[7]} direction={'left'}/>
     </Row>
     <ContactSection />
     </div>
